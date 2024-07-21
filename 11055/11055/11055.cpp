3cc74b17ecc94d25c4782729646aa2a62a7e91fd
carmen <luboy@naver.com>
2024-07-21 23:37:49 +0900

    Upload 11055

---
+++ 11055/11055/11055.cpp
@@ -0,0 +1,40 @@
+﻿// 11055.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
+//
+
+#include <iostream>
+using namespace std;
+
+int arr[1001];
+int dp[1001];
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    int n;
+    cin >> n;
+
+    for (int i = 0; i < n; i++) {
+        int num;
+        cin >> num;
+        arr[i] = num;
+        dp[i] = num;
+    }
+
+    int maxVal = dp[0] = arr[0];
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < i; j++) {
+            if (arr[i] > arr[j]) {
+                dp[i] = max(dp[i], dp[j] + arr[i]);
+            }
+        }
+
+        if (dp[i] > maxVal) {
+            maxVal = dp[i];
+        }
+    }
+
+    cout << maxVal;
+}
