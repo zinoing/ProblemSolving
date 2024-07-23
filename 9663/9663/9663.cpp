0af852775182e68a318d4d49bf8b0194608edf10
carmen <luboy@naver.com>
2024-07-23 17:05:18 +0900

    Update 9663

--- 9663/9663/9663.cpp
+++ 9663/9663/9663.cpp
@@ -6,77 +6,32 @@
 using namespace std;
 
 int N;
-bool board[15][15];
-bool previous[15][15][15];
+int col[15];
 int ans = 0;
 
-void dfs(pair<int,int> start) {
-
-    stack<pair<int, int>> s;
-    s.push(start);
-
-    while (!s.empty()) {
-        pair<int,int> q = s.top();
-        s.pop();
-
-        // 마지막 행에 대한 퀸일때
-        if (q.first == N-1) {
-            ++ans;
-            if (!s.empty()) {
-                // 보드를 이전 보드로 초기화 하기
-                for (int i = 0; i < N; i++) {
-                    for (int j = 0; j < N; j++) {
-                        board[i][j] = previous[s.top().first - 1][i][j];
-                    }
-                }
-            }
-            continue;
+bool check(int x) {
+    // 이전에 놓은 퀸들과 겹치지 않는지 체크
+    for (int j = 0; j < x; j++) {
+        // 열은 겹치지 않으니 행과 대각선만 체크하면 된다.
+        if (col[j] == col[x] || abs(col[x] - col[j]) == x - j) {
+            return false; // 겹칠 경우 다른 행을 가도록 지정
         }
+    }
+    return true;
+}
 
-        for (int i = 0; i < N; i++) {
-            // 열 설정
-            board[q.first][i] = false;
-            // 행 설정
-            board[i][q.second] = false;
+void solve(int x) {
+    if (x == N) {
+        ++ans;
+        return;
+    }
 
-            // 오른쪽 아래 대각선 설정
-            if(q.first + i !=  N && q.second + i != N)
-                board[q.first + i][q.second + i] = false;
-            // 왼쪽 위 대각선 설정
-            if (q.first - i >= 0 && q.second - i >= 0)
-                board[q.first - i][q.second - i] = false;
-            // 왼쪽 아래 대각선 설정
-            if (q.first + i != N && q.second - i >= 0)
-                board[q.first + i][q.second - i] = false;
-            // 오른쪽 위 대각선 설정
-            if (q.first - i >= 0 && q.second + i != N)
-                board[q.first - i][q.second + i] = false;
-        }
+    for (int i = 0; i < N; i++) {
+        // 퀸이 cnt번 열에서 몇 행을 갈지 지정
+        col[x] = i;
 
-        // back tracking을 위해 previous에 복사
-        for (int i = 0; i < N; i++) {
-            for (int j = 0; j < N; j++) {
-                previous[q.first][i][j] = board[i][j];
-            }
-        }
-
-        bool check = false;
-        for (int i = 0; i < N; i++) {
-            if (board[q.first + 1][i]) {
-                check = true;   
-                s.push(make_pair(q.first + 1, i));
-            }
-        }
-
-        // 다음에 퀸을 놓을 수 없다면
-        if (!check && !s.empty()) {
-            // 보드를 이전 보드로 초기화 하기
-            for (int i = 0; i < N; i++) {
-                for (int j = 0; j < N; j++) {
-                    board[i][j] = previous[s.top().first - 1][i][j];
-                }
-            }
-        }
+        if(check(x))
+            solve(x + 1);
     }
 }
 
@@ -88,20 +43,7 @@ int main()
 
     cin >> N;
 
-    for (int i = 0; i < N; i++) {
-
-        // 보드 초기화
-        for (int j = 0; j < N; j++) {
-            for (int k = 0; k < N; k++) {
-                board[j][k] = true;
-                for (int l = 0; l < N; l++) {
-                    previous[j][k][l] = true;
-                }
-            }
-        }
-
-        dfs(make_pair(0, i));
-    }
+    solve(0);
 
     cout << ans;
 }
