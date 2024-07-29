c1c2760f105056786613491ba6b044d083ebf48c
carmen <luboy@naver.com>
2024-07-30 01:00:31 +0900

    Update 1911 to solved version

--- Baekjoon/1911/1911/1911.cpp
+++ Baekjoon/1911/1911/1911.cpp
@@ -1,45 +1,41 @@
-﻿// 1911.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
-//
-
-#include <iostream>
+﻿#include <iostream>
 #include <vector>
 #include <algorithm>
 using namespace std;
 
-vector<pair<int,int>> pools;
-int ans;
-
-int main()
-{
+int main() {
     ios::sync_with_stdio(false);
     cin.tie(NULL);
-    cout.tie(NULL);
 
     int N, L;
     cin >> N >> L;
 
-    for (int i = 0; i < N; i++) {
-        int start, end;
-        cin >> start >> end;
-        pools.push_back(make_pair(start, end));
+    vector<pair<int, int>> pools(N);
+    for (int i = 0; i < N; ++i) {
+        cin >> pools[i].first >> pools[i].second;
     }
-    
+
+    // 웅덩이들을 시작 위치 기준으로 정렬
     sort(pools.begin(), pools.end());
 
-    for (int i = 0; i < N; i++) {
-        int dif = pools[i].second - pools[i].first;
+    int numPlanks = 0;
+    int coveredUntil = 0;
 
-        if (dif % L == 0) {
-            ans += (dif / L);
-            continue;
+    for (const auto& pool : pools) {
+        int start = pool.first;
+        int end = pool.second;
+
+        // 현재 널빤지로 커버할 수 있는 범위를 넘어서는 경우 새로운 널빤지를 배치
+        if (coveredUntil < start) {
+            coveredUntil = start;
         }
 
-        ans += (dif / L) + 1;
-
-        if (i < N-1 && pools[i].second + L - 2 >= pools[i + 1].first) {
-            pools[i+ 1].first = pools[i].second + L - 1;
+        while (coveredUntil < end) {
+            coveredUntil += L;  // 널빤지 하나 추가
+            numPlanks++;
         }
     }
 
-    cout << ans;
+    cout << numPlanks << "\n";
+    return 0;
 }
