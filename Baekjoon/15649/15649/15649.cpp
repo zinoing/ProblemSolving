b93699a541ac32a8922bdc33d76db5d6edcefe2b
carmen <luboy@naver.com>
2024-08-12 16:24:16 +0900

    Upload 15649

---
+++ Baekjoon/15649/15649/15649.cpp
@@ -0,0 +1,42 @@
+﻿// 15649.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
+//
+
+#include <iostream>
+#include <stack>
+#include <vector>
+using namespace std;
+
+int N, M;
+int arr[9];
+bool visited[9];
+int idx = 1;
+
+void dfs(int cnt) {
+    if (cnt == M) {
+        for (int i = 0; i < cnt; i++) {
+            cout << arr[i] << ' ';
+        }
+        cout << '\n';
+        return;
+    }
+
+    for(int i=1; i<= N; i++) {
+        if (!visited[i]) {
+            arr[cnt] = i;
+            visited[i] = true;
+            dfs(cnt + 1);
+            visited[i] = false;
+        }
+    }
+}
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    cin >> N >> M;
+    
+    dfs(0);
+}
