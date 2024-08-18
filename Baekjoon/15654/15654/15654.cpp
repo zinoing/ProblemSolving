c837bedfa49a5c9361decb433a02d9239e728b9c
carmen <luboy@naver.com>
2024-08-18 19:01:22 +0900

    Upload 15654

---
+++ Baekjoon/15654/15654/15654.cpp
@@ -0,0 +1,47 @@
+﻿// 15654.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
+//
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int N, M;
+bool visited[10000];
+int arr[9];
+vector<int> numbers;
+
+void dfs(int depth) {
+    if (depth == M) {
+        for (int i = 0; i < M; i++) {
+            cout << arr[i] << " ";
+        }
+        cout << '\n';
+        return;
+    }
+
+    for (int i = 0; i < N; i++) {
+        int number = numbers[i];
+        if (!visited[number]) {
+            arr[depth] = number;
+            visited[number] = true;
+            dfs(depth + 1);
+            arr[depth] = 0;
+            visited[number] = false;
+        }
+    }
+}
+
+int main()
+{
+    cin >> N >> M;
+    for (int i = 0; i < N; i++) {
+        int number;
+        cin >> number;
+        numbers.push_back(number);
+    }
+    sort(numbers.begin(), numbers.end());
+
+    dfs(0);
+}
