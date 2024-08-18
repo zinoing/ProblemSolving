209f4cc159908f8b296d7e402beda2a08669cdc0
carmen <luboy@naver.com>
2024-08-18 18:51:32 +0900

    Upload 15651

---
+++ Baekjoon/15651/15651/15651.cpp
@@ -0,0 +1,34 @@
+﻿// 15651.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
+//
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int N, M;
+int arr[9];
+
+void dfs(int depth) {
+    if (depth == M) {
+        for (int i = 0; i < M; i++) {
+            cout << arr[i] << " ";
+        }
+        cout << '\n';
+        return;
+    }
+
+    for (int i = 1; i <= N; i++) {
+
+        arr[depth] = i;
+        dfs(depth + 1);
+        arr[depth] = 0;
+    }
+}
+
+int main()
+{
+    cin >> N >> M;
+
+    dfs(0);
+}
