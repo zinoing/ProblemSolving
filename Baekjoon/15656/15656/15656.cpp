41c63bfd241f440da667c66324541b2287170084
carmen <luboy@naver.com>
2024-08-20 19:44:56 +0900

    Upload 15656

---
+++ Baekjoon/15656/15656/15656.cpp
@@ -0,0 +1,47 @@
+﻿// 15656.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
+//
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int N, M;
+int arr[9];
+vector<int> vec;
+
+void dfs(int depth) {
+    if (depth == M) {
+        for (int i = 0; i < M; i++) {
+            cout << arr[i] << ' ';
+        }
+        cout << '\n';
+        return;
+    }
+
+    for (int i = 1; i <= N; i++) {
+
+        int number = vec[i - 1];
+        arr[depth] = number;
+        dfs(depth + 1);
+    }
+}
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    cin >> N >> M;
+
+    for (int i = 1; i <= N; i++) {
+        int number;
+        cin >> number;
+        vec.push_back(number);
+    }
+
+    sort(vec.begin(), vec.end());
+
+    dfs(0);
+}
