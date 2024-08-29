3c9c617ea63f3f1220df982676a95251cfec2fc1
carmen <luboy@naver.com>
2024-08-29 22:45:31 +0900

    Upload 타겟 넘버

---
+++ Programmers/타겟 넘버/타겟 넘버/타겟 넘버.cpp
@@ -0,0 +1,26 @@
+﻿// 타겟 넘버.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
+//
+
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+void dfs(vector<int> numbers, int currentValue, int target, int cnt, int n, int* answer) {
+    if (cnt == n) {
+        if (currentValue == target) {
+            *answer += 1;
+        }
+        return;
+    }
+
+    dfs(numbers, currentValue + numbers[cnt], target, cnt + 1, n, answer);
+    dfs(numbers, currentValue - numbers[cnt], target, cnt + 1, n, answer);
+}
+
+int solution(vector<int> numbers, int target) {
+    int answer = 0;
+    dfs(numbers, 0, target, 0, numbers.size(), &answer);
+    return answer;
+}
