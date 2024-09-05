1515e05969ce37ae634e389223d82aefae377061
carmen <luboy@naver.com>
2024-09-05 18:22:49 +0900

    Upload 구명보트

---
+++ Programmers/구명보트/구명보트/구명보트.cpp
@@ -0,0 +1,26 @@
+﻿// 구명보트.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
+//
+
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int solution(vector<int> people, int limit) {
+    sort(people.begin(), people.end());
+    int answer = 0;
+
+    int numOfPeople = people.size();
+    int firstPerson = 0;
+    int lastPerson = numOfPeople - 1;
+    for (int i = lastPerson; i >= firstPerson; i--) {
+        if (people[firstPerson] + people[i] <= limit) {
+            ++firstPerson;
+        }
+        ++answer;
+    }
+
+    return answer;
+}
