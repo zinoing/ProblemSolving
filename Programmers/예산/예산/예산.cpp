4457c1f741677c1e4b85b8eaa10f0fd5414fd2a6
carmen <luboy@naver.com>
2024-10-16 00:51:29 +0900

    Upload 예산

---
+++ Programmers/예산/예산/예산.cpp
@@ -0,0 +1,27 @@
+﻿// 예산.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
+//
+
+#include <iostream>
+#include <stdio.h>
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int solution(vector<int> d, int budget) {
+    sort(d.begin(), d.end());
+
+    int sum = 0;
+    int answer = 0;
+    for (int i = 0; i < d.size(); ++i) {
+        if (sum + d[i] > budget) {
+            answer = i;
+            break;
+        }
+        else sum += d[i];
+    }
+    if (answer == 0 && sum != 0) answer = d.size();
+
+    return answer;
+}
