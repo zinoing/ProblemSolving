e906a9f6c634bda82daf4b55c8ae93a7915d5b66
carmen <luboy@naver.com>
2024-10-24 18:04:28 +0900

    Upload 11758

---
+++ Baekjoon/11758/11758/11758.cpp
@@ -0,0 +1,29 @@
+﻿// 11758.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
+//
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+struct Point {
+    int x;
+    int y;
+};
+
+int main()
+{
+    vector<Point> points(3);
+    float x, y;
+    for (int i = 0; i < 3; ++i) {
+        cin >> points[i].x >> points[i].y;
+    }
+
+    int crossProduct = (points[1].x - points[0].x) * (points[2].y - points[1].y)
+                        - (points[1].y - points[0].y) * (points[2].x - points[1].x);
+
+    if (crossProduct > 0) cout << 1;
+    else if (crossProduct == 0) cout << 0;
+    else cout << -1;
+    return 0;
+}
