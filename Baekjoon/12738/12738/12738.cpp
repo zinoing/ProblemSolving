ce40dca2eadbba4798f9f7ce7a6e184c65d8d3f2
carmen <luboy@naver.com>
2024-10-28 01:43:06 -0700

    Upload 12738

---
+++ Baekjoon/12738/12738/12738.cpp
@@ -0,0 +1,36 @@
+// 12738.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int lis(const vector<int>& numbers) {
+    vector<int> lis;
+
+    lis.push_back(numbers[0]);
+
+    for (int i = 1; i < numbers.size(); ++i) {
+        if (lis.back() < numbers[i]) {
+            lis.push_back(numbers[i]);
+        }
+        else {
+            lis[lower_bound(lis.begin(), lis.end(), numbers[i]) - lis.begin()] = numbers[i];
+        }
+    }
+
+    return lis.size();
+}
+
+int main()
+{
+    int n;
+    cin >> n;
+
+    vector<int> numbers(n, 0);
+    for (int i = 0; i < n; ++i) {
+        cin >> numbers[i];
+    }
+
+    cout << lis(numbers);
+}
