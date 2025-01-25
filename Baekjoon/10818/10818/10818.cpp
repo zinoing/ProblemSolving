fc790f758c525dbc05829a5d33ec40553b3ad9f8
carmen <luboy@naver.com>
2025-01-25 18:26:54 +0900

    Upload 10818

---
+++ Baekjoon/10818/10818/10818.cpp
@@ -0,0 +1,21 @@
+// 10818.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int main()
+{
+    int N;
+    cin >> N;
+
+    vector<int> nums(N);
+    for (int i = 0; i < N; ++i) {
+        cin >> nums[i];
+    }
+
+    cout << *min_element(nums.begin(), nums.end()) << " ";
+    cout << *max_element(nums.begin(), nums.end());
+}
