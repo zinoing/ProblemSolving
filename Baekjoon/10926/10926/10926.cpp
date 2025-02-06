5924878aad397c4a4a7add340deafe54851e66c4
carmen <luboy@naver.com>
2025-02-06 23:50:32 +0900

    Upload 10926

---
+++ Baekjoon/10926/10926/10926.cpp
@@ -0,0 +1,12 @@
+// 10926.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    string str;
+    cin >> str;
+    cout << str + "??!";
+}
