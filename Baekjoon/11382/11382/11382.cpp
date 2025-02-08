0d2081acddcc871b6163462d085291f520ae194e
carmen <luboy@naver.com>
2025-02-08 09:33:03 +0900

    Upload 11382

---
+++ Baekjoon/11382/11382/11382.cpp
@@ -0,0 +1,12 @@
+// 11382.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    long long A, B, C;
+    cin >> A >> B >> C;
+    cout << A + B + C;
+}
