5035665a240e31a2829993383c11e7250e309dac
carmen <luboy@naver.com>
2025-02-09 22:42:08 +0900

    Upload 2588

---
+++ Baekjoon/2588/2588/2588.cpp
@@ -0,0 +1,16 @@
+// 2588.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int a, b;
+    cin >> a >> b;
+
+    cout << a * (b % 10) << endl;
+    cout << a * ((b % 100) / 10) << endl;
+    cout << a * (b / 100) << endl;
+    cout << a * b << endl;
+}
